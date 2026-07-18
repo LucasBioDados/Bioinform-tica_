fda0ff130d2410f7017caa185b60b17a1423b265
LucasBioDados <lucasbiodados@hotmail.com>
2026-07-18 02:20:07 -0300

    Contador de resíduos sem site

--- Contador de residuo.py
+++ Contador de residuo.py
@@ -1,10 +1,4 @@
-import streamlit as st
-
-st. title ("Leitor de sequências proteícas")
-st. write("Insira abaixo sua sequência para saber a quantidade e porcentagem de cada resíduo ")
-
-
-sequencia = st.text_input("Digite aqui sua sequência de aminoácidos: ").upper().replace(" ", "")
+sequencia = input("Digite aqui sua sequência de aminoácidos: ").upper().replace(" ", "")
 total_aminoacidos = len(sequencia)
 contagem = {}
 for aa in sequencia: 
@@ -12,8 +6,8 @@ for aa in sequencia:
         contagem[aa] +=1
     else:
         contagem[aa] =1 
-st.subheader("Aqui está o quantitativo de resíduos da sua sequência: ")
+print("Aqui está o quantitativo de resíduos da sua sequência: ")
 for aa, valor in contagem.items():
     porcentagem = (valor/ total_aminoacidos)*100
     porcentagem_arredondada = round(porcentagem, 2)
-    st.write(f"Resíduo {aa}:{valor} percentual ({porcentagem_arredondada}%)")
+    print(f"Resíduo {aa}:{valor} percentual ({porcentagem_arredondada}%)")
